b612db9a44c4eb52c5f961e6c96d82d2fef36c48
Michael Rinehart <mjr426@drexel.edu>
2017-05-26 12:03:25 -0400

    Fixed Android Studio issue with overwriting friends_menu.java with portions of Friend_Result_Data.java

--- Application/SocialHour/app/src/main/java/socialhour/socialhour/friends_menu.java
+++ Application/SocialHour/app/src/main/java/socialhour/socialhour/friends_menu.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import socialhour.socialhour.adapter.FriendAdapter;
-import socialhour.socialhour.adapter.GroupAdapter;
 import socialhour.socialhour.model.FriendData;
 
 
